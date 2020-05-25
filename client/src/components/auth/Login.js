aec4dbd154c157e926d59dd6c1dabdf56664d807
reedandchrissy <lucicazhai@gmail.com>
2020-05-25 05:31:23 -0400

    build backend

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,90 @@
+import React, {Fragment, useState} from 'react';
+import {Link, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+//import {setAlert} from '../../actions/alert.js';
+
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth.js';
+
+const Login = ({login, isAuthenticated}) =>{
+    const [formData, setFormData] = useState({ 
+        email: '',
+        password: ''      
+    });
+    const {email, password} = formData;
+    const onChange = e => setFormData({
+        ...formData, [e.target.name]:e.target.value
+    });
+    const onSubmit = async e => {
+        e.preventDefault();
+        console.log("success");
+        console.log(login);
+        login(email, password);
+            /*
+            const newUser = {
+                name,
+                email,
+                password
+            }
+            try{
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                const body = JSON.stringify(newUser);
+                const res = await axios.post('/api/users', body, config);
+                console.log(res.data);
+            }catch(err){
+                console.error(err.response.data);  
+            }
+            */
+        
+    };   
+    // Redirect if logged in
+    if(isAuthenticated){
+        console.log("authend");
+        return <Redirect to="dashboard" />;
+    }else{
+        console.log("no auth")
+    }
+    return <Fragment>
+        <h1 className="large text-primary">Sign In</h1>
+        <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+        <form className="form" onSubmit={e=> onSubmit(e)} >
+            <div className="form-group">
+            <input
+                type="email"
+                placeholder="Email Address"
+                name="email"
+                value={email} 
+                onChange={e=> onChange(e)}
+                required
+            />
+            </div>
+            <div className="form-group">
+            <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password} 
+                onChange={e=> onChange(e)}
+            />
+            </div>
+            <input type="submit" className="btn btn-primary" value="Login" />
+        </form>
+        <p className="my-1">
+            Don't have an account? <Link to="/register">Sign Up</Link>
+        </p>
+    </Fragment>
+};
+Login.propTypes = {
+    
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
+const mapStateToProps = state =>({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, {login})(Login);
---
+++ client/src/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import alert from './alert.js';
+import auth from './auth.js';
+import profile from './profile.js';
+export default combineReducers({
+    alert,
+    auth,
+    profile
+});
